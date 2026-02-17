9b42ee6c79fc96a15229efafa77066844075e032
Luis Lopes <luismvlopes@gmail.com>
2026-02-17 13:24:50 +0000

    Adds details to  Swagger

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -4,12 +4,15 @@ import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { UseGuards, Get, Request } from '@nestjs/common';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get('me')
   getProfile(@Request() req) {
